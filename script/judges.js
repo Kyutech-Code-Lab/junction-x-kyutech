727b9476be5e6f9b995ba42a0b0989ea45e60f8d
U5 <kyutech.joko@gmail.com>
2026-09-14 20:22:05 +0900

    審査員を姓のアルファベット順に変更

--- script/judges.js
+++ script/judges.js
@@ -13,22 +13,22 @@ const judgesData = [
   },
   {
     name: {
-      en: "Mr. Rin Sato",
-      ja: "佐藤 凜氏",
-    },
-    role: {
-      en: "Co-Founder & CEO, Kick Space Technologies Inc.",
-      ja: "キックスペーステクノロジーズ株式会社<br>代表取締役CEO",
-    },
-  },
-  {
-    name: {
       en: "Mr. Takaomi Muraoka",
       ja: "村岡 剛臣氏",
     },
     role: {
       en: "General Manager, Digital Engineering Div., TOYOTA Body Research & Development Co., Ltd.",
       ja: "株式会社トヨタ車体研究所<br>デジタルエンジニアリング部 部長",
+    },
+  },
+  {
+    name: {
+      en: "Mr. Rin Sato",
+      ja: "佐藤 凜氏",
+    },
+    role: {
+      en: "Co-Founder & CEO, Kick Space Technologies Inc.",
+      ja: "キックスペーステクノロジーズ株式会社<br>代表取締役CEO",
     },
   },
   {
